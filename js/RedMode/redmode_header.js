9f6971bff94f9c0045f977492a6601b2bcdeb642
ie1w3n <Ohshjt125@gmail.com>
2026-04-19 19:10:39 +0700

    Update redmode_header.js

--- js/RedMode/redmode_header.js
+++ js/RedMode/redmode_header.js
@@ -1,3 +1,9 @@
+/**
+ * @name RedMode Header Fix
+ * @author Xwuan
+ * @description Delete ETag to bypass RevenueCat cache
+ */
+
 var modifiedHeaders = $request.headers;
 var eTag = "X-RevenueCat-ETag";
 var eTagLower = "x-revenuecat-etag";
@@ -5,4 +11,5 @@ var eTagLower = "x-revenuecat-etag";
 if (modifiedHeaders[eTag]) { modifiedHeaders[eTag] = ""; }
 if (modifiedHeaders[eTagLower]) { modifiedHeaders[eTagLower] = ""; }
 
+console.log("RedMode Header Fix by Xwuan Applied");
 $done({headers: modifiedHeaders});
