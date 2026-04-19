d570fde24e31c2ebb91dc235249a91702199e4cb
ie1w3n <Ohshjt125@gmail.com>
2026-04-19 19:00:47 +0700

    Create redmode_sub.js

---
+++ js/RedMode/redmode_sub.js
@@ -0,0 +1,26 @@
+var obj = JSON.parse($response.body);
+
+obj.Attention = "RedMode Premium đã kích hoạt! Chúc bạn dùng vui vẻ.";
+
+var redmode_data = {
+  "is_sandbox": false,
+  "ownership_type": "PURCHASED",
+  "billing_issues_detected_at": null,
+  "period_type": "normal",
+  "expires_date": "2099-12-31T23:59:59Z",
+  "grace_period_expires_date": null,
+  "unsubscribe_detected_at": null,
+  "original_purchase_date": "2024-01-01T00:00:00Z",
+  "purchase_date": "2024-01-01T00:00:00Z",
+  "store": "app_store"
+};
+
+obj.subscriber.subscriptions["com.redmode.pro"] = redmode_data;
+obj.subscriber.entitlements["pro"] = {
+  "grace_period_expires_date": null,
+  "purchase_date": "2024-01-01T00:00:00Z",
+  "product_identifier": "com.redmode.pro",
+  "expires_date": "2099-12-31T23:59:59Z"
+};
+
+$done({body: JSON.stringify(obj)});
