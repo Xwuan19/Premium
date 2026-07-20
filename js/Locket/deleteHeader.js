9a7d4968521ea9671b20521517d753c4e74e6327
ohshjt125 <Ohshjt125@gmail.com>
2026-07-20 14:27:59 +0700

    Update deleteHeader.js

--- js/Locket/deleteHeader.js
+++ js/Locket/deleteHeader.js
@@ -1,11 +1,17 @@
-const version = 'V1.0.3';
-
-function setHeaderValue(e, a, d) {
-  var r = a.toLowerCase();
-  r in e ? e[r] = d : e[a] = d;
-}
-
-var modifiedHeaders = $request.headers;
-setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
-console.log("Modified Headers:", JSON.stringify(modifiedHeaders));
-$done({ headers: modifiedHeaders });
+// deleteHeader.js - NightmarketServer
+// Updated deleteHeader.js
+let headers = $request.headers;
+// Xóa token và header đặc trưng của Locket/RevenueCat
+delete headers["Authorization"];
+delete headers["X-Platform"];
+delete headers["X-Version"];
+delete headers["X-Client-Build"];
+delete headers["X-Client-Date"];
+delete headers["X-Client-Timezone"];
+delete headers["X-Device-Id"];
+delete headers["X-App-Build"];
+delete headers["X-Storefront"];
+delete headers["If-None-Match"];
+// Giả mạo User-Agent iOS
+headers["User-Agent"] = "Locket/1.0 CFNetwork/1404.0.5 Darwin/22.3.0";
+$done({ headers: headers });
